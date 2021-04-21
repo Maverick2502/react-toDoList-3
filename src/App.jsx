9c4630e2184e7bdc780f09c915f7601e120cd958
Nurullo <khuwaidullaev@gmail.com>
2021-04-21 14:06:30 +0500

    Added Delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoList todos={todos} />
+      <TodoList setTodos={setTodos} todos={todos} />
       <AddToDo setTodos={setTodos} />
     </div>
   );
@@ -29,6 +29,10 @@ function TodoList({ todos, setTodos }) {
     setTodos(updatedTodos);
   }
 
+  if (!todos.length) {
+    return <p>No todos left!</p>;
+  }
+
   return (
     <ul>
       {todos.map((todo) => (
@@ -38,15 +42,41 @@ function TodoList({ todos, setTodos }) {
           key={todo.id}
         >
           {todo.text}
-          {/* <DeleteTodo todo={todo} setTodos={setTodos} /> */}
+          <DeleteTodo todo={todo} setTodos={setTodos} />
         </li>
       ))}
     </ul>
   );
 }
 
+function DeleteTodo({ todo, setTodos }) {
+  function handleDeleteTodo() {
+    const confirmed = window.confirm("Do you want to delete this?");
+    if (confirmed) {
+      setTodos((prevTodos) => {
+        return prevTodos.filter((t) => t.id !== todo.id);
+      });
+    }
+  }
+
+  return (
+    <span
+      onClick={handleDeleteTodo}
+      role="button"
+      style={{
+        color: "red",
+        fontWeight: "bold",
+        marginLeft: 10,
+        cursor: "pointer",
+      }}
+    >
+      x
+    </span>
+  );
+}
+
 function AddToDo({ setTodos }) {
-  const inputRef = React.useRef();
+  const inputRef = React.useRef(); //for clearing out input's value
   function handleAddToDo(e) {
     e.preventDefault();
     const text = e.target.elements.addTodo.value;
